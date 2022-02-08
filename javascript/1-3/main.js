c8dd6150cedef3cfba8bd41aa9ac06b21f9f63dc
kazu08291991 <98957224+kazu08291991@users.noreply.github.com>
2022-02-08 20:12:21 +0900

    1-3
    提出用

---
+++ javascript/1-3/main.js
@@ -0,0 +1,16 @@
+a != 10;
+b >= 10 &&  b <= 20 && a % 2 === 0;
+
+let x = 10
+if (x >= 10) {
+    console.log('成功です');
+} else {
+    console.log('失敗です');
+}
+
+let y = 0
+if (y % 2 === 0) {
+    console.log('偶数です');
+} else if(y % 2 !== 0) {
+    console.log('奇数です');
+}
